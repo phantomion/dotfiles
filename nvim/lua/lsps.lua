fe67343e7da1afff292af10a67fe8a4cc106980e
DarkPhantom <konstantinkyr@protonmail.com>
2021-06-20 14:12:16 +0300

    LspInstall functions

--- nvim/lua/lsps.lua
+++ nvim/lua/lsps.lua
@@ -4,77 +4,70 @@ local on_attach_vim = function(client)
     nvim_command("autocmd CursorHold * lua require'lspsaga.diagnostic'.show_line_diagnostics()")
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-require'lspinstall'.setup() -- important
+local function make_config()
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+  return {
+    -- enable snippet support
+    capabilities = capabilities,
+    -- map buffer local keybindings when the language server attaches
+    on_attach = on_attach_vim,
+  }
+end
 
---language servers
-require'lspconfig'.rust_analyzer.setup{
-    on_attach = on_attach_vim,
-    capabilities = capabilities,
-}
-require'lspconfig'.clangd.setup{
-    cmd = { "clangd", "--background-index", "--suggest-missing-includes",
-        "--all-scopes-completion", "--completion-style=detailed" },
-    on_attach = on_attach_vim,
-    capabilities = capabilities,
-}
-require'lspconfig'.clojure_lsp.setup{
-    capabilities = capabilities,
-    on_attach = on_attach_vim,
-}
-require'lspconfig'.nimls.setup{
-    on_attach = on_attach_vim,
-    capabilities = capabilities,
-}
-require'lspconfig'.elixirls.setup{on_attach=on_attach_vim}
-require'lspconfig'.vuels.setup{
-    on_attach = on_attach_vim,
-    capabilities = capabilities,
-    settings = {
-        vetur = {
-            completion = {
-                autoImport = true;
-                useScaffoldSnippets = true;
-            }
-        }
-    }
-}
-require'lspconfig'.texlab.setup{
-    on_attach = on_attach_vim,
-    capabilities = capabilities,
-    settings = {
-        latex = {
-            build = {
-                args = { "-lualatex", "-interaction=nonstopmode", "-synctex=1", "%f" };
-                executable = "latexmk";
-                onSave = true;
-            },
-            lint = {
-                onChange = true
-            }
-        }
-    }
-}
-require'lspconfig'.tsserver.setup{on_attach=on_attach_vim}
 require'lspconfig'.pyls.setup{
     capabilities = capabilities,
     on_attach = on_attach_vim,
 }
-require'lspconfig'.intelephense.setup{
-    capabilities = capabilities,
-    on_attach = on_attach_vim,
-}
-require'lspconfig'.html.setup{
-    capabilities = capabilities,
-    on_attach = on_attach_vim,
-}
-require'lspconfig'.css.setup{
-    capabilities = capabilities,
-    on_attach = on_attach_vim,
-}
-require'lspconfig'.hls.setup{on_attach=on_attach_vim}
+
+local function setup_servers()
+    require'lspinstall'.setup()
+    local servers = require'lspinstall'.installed_servers()
+
+    for _, server in pairs(servers) do
+        --print(server)
+        local config = make_config()
+        if server == "clangd" then
+            config.cmd = { "clangd", "--background-index", "--suggest-missing-includes",
+            "--all-scopes-completion", "--completion-style=detailed" }
+        end
+        if server == "vuels" then
+            config.settings = {
+                vetur = {
+                    completion = {
+                        autoImport = true;
+                        useScaffoldSnippets = true;
+                    }
+                }
+            }
+        end
+        if server == "texlab" then
+            config.settings = {
+                latex = {
+                    build = {
+                        args = { "-lualatex", "-interaction=nonstopmode", "-synctex=1", "%f" };
+                        executable = "latexmk";
+                        onSave = true;
+                    },
+                    lint = {
+                        onChange = true
+                    }
+                }
+            }
+        end
+        require'lspconfig'[server].setup(config)
+    end
+end
+
+setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require'lspinstall'.post_install_hook = function ()
+    setup_servers() -- reload installed servers
+    vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+end
+
 
 local actions = require('telescope.actions')
 require'telescope'.setup{
