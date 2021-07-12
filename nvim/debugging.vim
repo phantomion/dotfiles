9abf4941437ba4e43135f8dc7d65b991af74ca12
DarkPhantom <konstantinkyr@protonmail.com>
2021-07-12 11:55:26 +0300

    Remove unused files

--- nvim/debugging.vim
+++
@@ -1,9 +0,0 @@
-nnoremap <silent> <leader>dc :lua require'dap'.continue()<CR>
-nnoremap <silent> <leader>ss :lua require'dap'.step_over()<CR>
-nnoremap <silent> <leader>si :lua require'dap'.step_into()<CR>
-nnoremap <silent> <leader>so :lua require'dap'.step_out()<CR>
-nnoremap <silent> <leader>db :lua require'dap'.toggle_breakpoint()<CR>
-nnoremap <silent> <leader>dB :lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>
-nnoremap <silent> <leader>lp :lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>
-nnoremap <silent> <leader>dr :lua require'dap'.repl.open()<CR>
-nnoremap <silent> <leader>dl :lua require'dap'.run_last()<CR>
--- nvim/lua/plugin_settings.lua
+++ nvim/lua/plugin_settings.lua
@@ -1,5 +1,7 @@
 -- Nvim tree
-vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+local remap = vim.api.nvim_set_keymap
+
+remap('n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
 
 vim.g.nvim_tree_width = 27
 vim.g.nvim_tree_indent_markers = 1
@@ -13,56 +15,56 @@ vim.g.vimwiki_text_ignore_newline = 0
 vim.g.vimwiki_table_mappings = 0
 
 -- Barbar
-vim.api.nvim_set_keymap('n', '<C-s>', ':BufferClose<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-e>', ':BufferNext<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-q>', ':BufferPrevious<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>1', ':BufferGoto 1<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>2', ':BufferGoto 2<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>3', ':BufferGoto 3<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>4', ':BufferGoto 4<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>5', ':BufferGoto 5<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>6', ':BufferGoto 6<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>7', ':BufferGoto 7<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>8', ':BufferGoto 8<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>9', ':BufferGoto 9<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>0', ':BufferLast<CR>', {noremap = true, silent = true})
+remap('n', '<C-s>', ':BufferClose<CR>', {noremap = true, silent = true})
+remap('n', '<C-e>', ':BufferNext<CR>', {noremap = true, silent = true})
+remap('n', '<C-q>', ':BufferPrevious<CR>', {noremap = true, silent = true})
+remap('n', '<leader>1', ':BufferGoto 1<CR>', {noremap = true, silent = true})
+remap('n', '<leader>2', ':BufferGoto 2<CR>', {noremap = true, silent = true})
+remap('n', '<leader>3', ':BufferGoto 3<CR>', {noremap = true, silent = true})
+remap('n', '<leader>4', ':BufferGoto 4<CR>', {noremap = true, silent = true})
+remap('n', '<leader>5', ':BufferGoto 5<CR>', {noremap = true, silent = true})
+remap('n', '<leader>6', ':BufferGoto 6<CR>', {noremap = true, silent = true})
+remap('n', '<leader>7', ':BufferGoto 7<CR>', {noremap = true, silent = true})
+remap('n', '<leader>8', ':BufferGoto 8<CR>', {noremap = true, silent = true})
+remap('n', '<leader>9', ':BufferGoto 9<CR>', {noremap = true, silent = true})
+remap('n', '<leader>0', ':BufferLast<CR>', {noremap = true, silent = true})
 
 -- Symbols outline
-vim.api.nvim_set_keymap('n', '<C-b>', ':SymbolsOutline<CR>', {noremap = true, silent = true})
+remap('n', '<C-b>', ':SymbolsOutline<CR>', {noremap = true, silent = true})
 
 -- Neogit
-vim.api.nvim_set_keymap('n', '<leader>ng', ':Neogit<CR>', {noremap = true, silent = true})
+remap('n', '<leader>ng', ':Neogit<CR>', {noremap = true, silent = true})
 -- Treesitter
-vim.api.nvim_set_keymap('n', '<leader>rr', ':w<CR>:e<CR>:TSBufEnable highlight<CR>', {noremap = true, silent = true})
+remap('n', '<leader>rr', ':w<CR>:e<CR>:TSBufEnable highlight<CR>', {noremap = true, silent = true})
 
 vim.g.rainbow_active = 1
 vim.g.signify_sign_change = '~'
 
 -- lsp
-vim.api.nvim_set_keymap('n', '<leader>gd', ':lua vim.lsp.buf.definition()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>gi', ':lua vim.lsp.buf.implementation()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>gh', ":lua require('lspsaga.signaturehelp').signature_help()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>gr', ':lua vim.lsp.buf.references()<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>rn', ":lua require('lspsaga.rename').rename()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>prn', "viw:lua require('spectre').open_visual()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>S', ":lua require('spectre').open()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<A-m>', ":lua require('lspsaga.hover').render_hover_doc()", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>ga', ":lua require('lspsaga.codeaction').code_action()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-p>', ":lua require'telescope.builtin'.git_files{}<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>pp', ":lua require'telescope.builtin'.find_files{}<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>pw', ":lua require'telescope.builtin'.grep_string { search = vim.fn.expand('<cword>') }<CR><CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>ps', ":lua require('telescope.builtin').grep_string({ search = vim.fn.input('Grep For > ')})<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>]', ":lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_next()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>[', ":lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>dg', ":lua vim.lsp.diagnostic.set_loclist()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fm', ":lua vim.lsp.buf.formatting()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-f>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-b>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>", {noremap = true, silent = true})
+remap('n', '<leader>gd', ':lua vim.lsp.buf.definition()<CR>', {noremap = true, silent = true})
+remap('n', '<leader>gi', ':lua vim.lsp.buf.implementation()<CR>', {noremap = true, silent = true})
+remap('n', '<leader>gh', ":lua require('lspsaga.signaturehelp').signature_help()<CR>", {noremap = true, silent = true})
+remap('n', '<leader>gr', ':lua vim.lsp.buf.references()<CR>', {noremap = true, silent = true})
+remap('n', '<leader>rn', ":lua require('lspsaga.rename').rename()<CR>", {noremap = true, silent = true})
+remap('n', '<leader>prn', "viw:lua require('spectre').open_visual()<CR>", {noremap = true, silent = true})
+remap('n', '<leader>S', ":lua require('spectre').open()<CR>", {noremap = true, silent = true})
+remap('n', '<A-m>', ":lua require('lspsaga.hover').render_hover_doc()<CR>", {noremap = true, silent = true})
+remap('n', '<leader>ga', ":lua require('lspsaga.codeaction').code_action()<CR>", {noremap = true, silent = true})
+remap('n', '<C-p>', ":lua require'telescope.builtin'.git_files{}<CR>", {noremap = true, silent = true})
+remap('n', '<leader>pp', ":lua require'telescope.builtin'.find_files{}<CR>", {noremap = true, silent = true})
+remap('n', '<leader>pw', ":lua require'telescope.builtin'.grep_string { search = vim.fn.expand('<cword>') }<CR><CR>", {noremap = true, silent = true})
+remap('n', '<leader>ps', ":lua require('telescope.builtin').grep_string({ search = vim.fn.input('Grep For > ')})<CR>", {noremap = true, silent = true})
+remap('n', '<leader>]', ":lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_next()<CR>", {noremap = true, silent = true})
+remap('n', '<leader>[', ":lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev()<CR>", {noremap = true, silent = true})
+remap('n', '<leader>dg', ":lua vim.lsp.diagnostic.set_loclist()<CR>", {noremap = true, silent = true})
+remap('n', '<leader>fm', ":lua vim.lsp.buf.formatting()<CR>", {noremap = true, silent = true})
+remap('n', '<C-f>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>", {noremap = true, silent = true})
+remap('n', '<C-b>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>", {noremap = true, silent = true})
 
-vim.api.nvim_set_keymap('i', '<C-Space>', 'compe#complete()', {noremap = true, silent = true, expr = true})
-vim.api.nvim_set_keymap('i', '<CR>', "compe#confirm('<CR>')", {noremap = true, silent = true, expr = true})
-vim.api.nvim_set_keymap('i', '<C-e>', "compe#close('<C-e>')", {noremap = true, silent = true, expr = true})
-vim.api.nvim_set_keymap('i', '<C-f>', "compe#scroll({ 'delta': +4 })", {noremap = true, silent = true, expr = true})
-vim.api.nvim_set_keymap('i', '<C-d>', "compe#scroll({ 'delta': -4 })", {noremap = true, silent = true, expr = true})
+remap('i', '<C-Space>', 'compe#complete()', {noremap = true, silent = true, expr = true})
+remap('i', '<CR>', "compe#confirm('<CR>')", {noremap = true, silent = true, expr = true})
+remap('i', '<C-e>', "compe#close('<C-e>')", {noremap = true, silent = true, expr = true})
+remap('i', '<C-f>', "compe#scroll({ 'delta': +4 })", {noremap = true, silent = true, expr = true})
+remap('i', '<C-d>', "compe#scroll({ 'delta': -4 })", {noremap = true, silent = true, expr = true})
 
 vim.cmd([[ highlight link CompeDocumentation NormalFloat ]])
